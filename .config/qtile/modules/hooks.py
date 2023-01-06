deade8e234ce785940d5cfde32fcd013c13958f6
kaspar <kaspar.wolfisberg@gmail.com>
2023-01-06 04:22:23 +0100

    Create layouting hook for screens

--- .config/qtile/modules/hooks.py
+++ .config/qtile/modules/hooks.py
@@ -1,10 +1,22 @@
 import os
 import subprocess
 
-from libqtile import hook
+from libqtile import hook, qtile
+
+from modules.consts import SCREEN_LEFT
+from modules.layouts import LAYOUT_VERTICAL_TILE, LAYOUT_MONAD_TALL
 
 
 @hook.subscribe.startup_once
 def autostart():
     home = os.path.expanduser('~/.config/qtile/autostart.sh')
     subprocess.call([home])
+
+
+@hook.subscribe.setgroup
+def set_layout_per_screen():
+    for s in qtile.screens:
+        if s.index == SCREEN_LEFT:
+            s.group.cmd_setlayout(LAYOUT_VERTICAL_TILE.name)
+        else:
+            s.group.cmd_setlayout(LAYOUT_MONAD_TALL.name)
--- .config/qtile/modules/todo.py
+++
@@ -1,4 +0,0 @@
-# TODO: assign groups
-# TODO: add three col monad
-# TODO: setup default layouts (for vert monitor), can you force layout on monitor?
-# TODO: setup sound
