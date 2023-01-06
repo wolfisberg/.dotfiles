70e70a0ca995cd5c03acce291beaba5a111f3110
kaspar <kaspar.wolfisberg@gmail.com>
2023-01-06 23:21:19 +0100

    Update todos

--- .config/qtile/modules/todo.py
+++ .config/qtile/modules/todo.py
@@ -1,3 +1,6 @@
 # TODO: on launching client, focus
 # TODO: make rofi spawn on main monitor only
-# TODO: apply xrandr on login screen (lightdm?)
+# TODO: spotify widget in bar
+# TODO: alt+tab? (rofi?)
+# TODO: keys / super / trapped in terminal etc?
+# TODO: show MAX layout border
