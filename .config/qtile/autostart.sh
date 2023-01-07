c6673c334bf47ce0d68bfdec9d24f40e81c63608
kaspar <kaspar.wolfisberg@gmail.com>
2023-01-07 18:32:31 +0100

    Clean up

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -20,9 +20,6 @@ picom & disown # --experimental-backends --vsync should prevent screen tearing o
 # Low battery notifier
 ~/.config/qtile/scripts/check_battery.sh & disown
 
-# Start welcome
-#eos-welcome & disown
-
 # Policy kit
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & disown # start polkit agent from GNOME
 
--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -6,10 +6,6 @@ from modules.mouse import mouse
 from modules.hooks import *
 
 
-# dgroups_key_binder = None
-# dgroups_app_rules = []
-# sendtogroupkeys = dict(font="Cascadia Code", fontsize=13, padding=3)
-
 main = None  # WARNING: this is deprecated and will be removed soon
 follow_mouse_focus = True
 bring_front_click = False
