25a2554a9a7bd0ae1a9dd5f9adedf737c6ad6eff
kaspar <kaspar.wolfisberg@gmail.com>
2023-01-07 03:07:58 +0100

    Update todos

--- .config/qtile/modules/todo.py
+++ .config/qtile/modules/todo.py
@@ -1,8 +1,8 @@
-# TODO: on launching client, focus
-# TODO: make rofi spawn on main monitor only
-# TODO: alt+tab? (rofi?)
-# TODO: keys / super / trapped in terminal etc?
 # TODO: save prev layout when switching to 3 and restore when moving out of 3
+# TODO: make rofi spawn on main monitor only
+# TODO: keys / super / trapped in terminal etc?
+# TODO: alt+tab? (rofi?)
+# TODO: on launching client, focus
 # TODO: write (go?) service (systemd?) to listen for mpris:artUrl
 # on dbus and set wallpapers
 # dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'
