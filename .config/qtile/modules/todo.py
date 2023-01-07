834cf33b602bb073fe5132c9e157b5daefcc9fea
kaspar <kaspar.wolfisberg@gmail.com>
2023-01-07 03:42:42 +0100

    Update todos

--- .config/qtile/modules/todo.py
+++ .config/qtile/modules/todo.py
@@ -1,3 +1,5 @@
+# TODO: Scratchpads (terminal, keepass, filemanager?)
+# TODO: remove mouse accel
 # TODO: save prev layout when switching to 3 and restore when moving out of 3
 # TODO: make rofi spawn on main monitor only
 # TODO: keys / super / trapped in terminal etc?
@@ -6,3 +8,6 @@
 # TODO: write (go?) service (systemd?) to listen for mpris:artUrl
 # on dbus and set wallpapers
 # dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'
+# TODO: set keybind for minimizing windows
+# Key([MOD, CTRL], "space", lazy.layout.minimize(), desc="minimize window"),
+# https://docs.qtile.org/en/latest/manual/ref/commands.html#libqtile.backend.base.Window.toggle_minimize
